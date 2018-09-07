62ad4d72cbc222a22ce1528114c0a922ddac1261
gggmt <34018741+gmt710@users.noreply.github.com>
2018-09-07 10:59:56 +0800

    Create singleNumber.py
    只出现一次的数字

---
+++ primary/singleNumber.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_times=collections.Counter(nums)
+        for k,v in nums_times.items():
+            if v == 1:
+                return k
