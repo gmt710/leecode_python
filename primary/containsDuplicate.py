05e48bcba8ec6dcf5b8f86c8e4ef14ec4ba9b0ff
gggmt <34018741+gmt710@users.noreply.github.com>
2018-09-07 10:59:07 +0800

    Create containsDuplicate.py
    存在重复

---
+++ primary/containsDuplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums_times=collections.Counter(nums)
+        for i in nums_times.values():
+            if i > 1:
+                return True
+        return False
+            
