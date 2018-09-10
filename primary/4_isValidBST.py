cdc56f95c24fff3ae620ba8121b4715036369dc4
gggmt <34018741+gmt710@users.noreply.github.com>
2018-09-10 10:12:05 +0800

    验证二叉搜索树

---
+++ primary/4_isValidBST.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def validBST(self,root,small,large):
+        if root==None:
+            return True
+        if small>=root.val or large<=root.val:
+            return False  
+        return self.validBST(root.left,small,root.val) and self.validBST(root.right,root.val,large)
+    
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.validBST(root,-2**32,2**32-1)
+    
+            
