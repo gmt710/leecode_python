bc9737b0136d028bf7510b43cadbc1961b4e033b
gggmt <34018741+gmt710@users.noreply.github.com>
2018-09-25 11:48:39 +0800

    数组中的第K个最大元素

---
+++ middle/5_findKthLargest.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        aa = sorted(nums)
+        aa.reverse()
+        return aa[k-1]
