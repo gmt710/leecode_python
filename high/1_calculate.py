2dac1c062ec3ad44210a2d1e8816c5b13189a3ab
gggmt <1065504814@qq.com>
2018-11-28 17:38:04 +0800

    Update 1_calculate.py

--- high/1_calculate.py
+++ high/1_calculate.py
@@ -17,8 +17,10 @@ class Solution(object):
         pre_op = '+'
         num = 0
         for i, each in enumerate(s):
+            # 如果是数字的话，就先给num
             if each.isdigit():
                 num = 10 *num + int(each)
+            # 如果是最后一个数，那么将之间的运算符来计算；如果是运算符，则给pre_op
             if i == len(s) - 1 or each in '+-*/':
                 if pre_op == '+':
                     stack.append(num)
@@ -28,15 +30,18 @@ class Solution(object):
                     stack.append(stack.pop()*num)
                 elif pre_op == '/':
                     top = stack.pop()
-                    print(top)
                     if top < 0:
+                        # 如果为-，先取绝对值后再取反
                         stack.append(-(abs(top)/num))
                     else:
+                        # 直接取整
                         stack.append(top // num)
-                        
+                # 如果是运算符，则给pre_op
                 pre_op = each
+                # 将num赋值为0，用于存下一个数
                 num = 0
         
+        # 所有的数都是正数的存进去的
         return sum(stack)
             
         
