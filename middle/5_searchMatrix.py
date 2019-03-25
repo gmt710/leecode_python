a91140a55a0c9310bab46a90eaea7fae6471da62
gggmt <1065504814@qq.com>
2019-03-25 21:37:54 +0800

    Update 5_searchMatrix.py

--- middle/5_searchMatrix.py
+++ middle/5_searchMatrix.py
@@ -11,3 +11,25 @@ class Solution(object):
         return False
         
                     
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if len(matrix) == 0 or len(matrix[0]) == 0: return False
+        
+        row = len(matrix)
+        col = len(matrix[0])
+        r = row-1
+        c = 0
+        
+        while r >= 0 and c < col:
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] > target:
+                r -= 1
+            else:
+                c += 1
+        return False
