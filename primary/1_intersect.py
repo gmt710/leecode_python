1fdf312572fb078416155d4a3d8a62c193d7b575
gggmt <1065504814@qq.com>
2019-03-28 13:55:01 +0800

    Update 1_intersect.py

--- primary/1_intersect.py
+++ primary/1_intersect.py
@@ -24,3 +24,23 @@ class Solution(object):
         return nums
         
         
+class Solution:
+#     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         nums = []
+#         len_nums1 = len(nums1)
+#         len_nums2 = len(nums2)
+#         lens = min(len_nums1, len_nums2)
+#         count_nums1 = collections.Counter(nums1)
+#         count_nums2 = collections.Counter(nums2)
+#         for i in range(lens):
+#             if len_nums1 <= len_nums2:
+#                 if nums1[i] in nums2[:] and count_nums2[nums1[i]]:
+#                     nums.append(nums1[i])
+#                     count_nums2[nums1[i]] -= 1
+#             else:
+#                 if nums2[i] in nums1[:] and count_nums1[nums2[i]]:
+#                     nums.append(nums2[i])
+#                     count_nums1[nums2[i]] -= 1
+#                     print(nums)
+#         return nums[:]
+    
