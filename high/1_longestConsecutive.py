5b47ad1da2d7a4509824b2964e473d6e4aaf6cb9
gggmt <1065504814@qq.com>
2019-04-11 19:26:24 +0800

    Update 1_longestConsecutive.py

--- high/1_longestConsecutive.py
+++ high/1_longestConsecutive.py
@@ -33,3 +33,19 @@ class Solution(object):
 
         
         
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        dp = [1] * len(nums)
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]+1:
+                dp[i] += dp[i-1]
+            elif nums[i] == nums[i-1]:
+                dp[i] = dp[i-1]
+            else:
+                dp[i] = 1
+        return max(dp[:])
+                
+        
